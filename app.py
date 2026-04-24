b3402f067aca0a4d81cc80fcc43154964f653fec
Htet Wai Aung <zawzaw781224@gmail.com>
2026-04-24 18:36:16 +0630

    fix dependencies + template update

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from datetime import date    # ✅ ဒီလို uncomment လုပ်ထားရမယ်
-from flask import Flask, render_template
+from datetime import date
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    start = date(2022, 12, 14)  # anniversary date
+    start = date(2023, 1, 3)
     today = date.today()
 
     delta = today - start
@@ -14,10 +17,39 @@ def home():
     months = (days % 365) // 30
     rem_days = (days % 365) % 30
 
-    return render_template("index.html", years=years, months=months, days=rem_days)
+    return render_template("index.html",
+                           years=years,
+                           months=months,
+                           days=rem_days)
 
-import os
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username=? AND password=?",
+            (username, password)
+        )
+
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            return redirect(url_for("home"))
+        else:
+            return "Wrong username or password"
+
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render gives PORT env variable
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
---
+++ database.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+id INTEGER PRIMARY KEY,
+username TEXT,
+password TEXT
+)
+""")
+
+cursor.execute(
+"INSERT INTO users (username,password) VALUES (?,?)",
+("admin","312023")
+)
+
+conn.commit()
+conn.close()
+
+print("database created")
